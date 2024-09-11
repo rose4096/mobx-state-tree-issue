3ad7f5dcc290ae471620285da8cd79111392331a
rose4096 <>
2024-09-11 17:02:01 -0400

    Fix test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import {useMst} from "@/app/mst";
+import {observer} from "mobx-react-lite";
 import {useEffect} from "react";
 
-export default function Home() {
+
+const Home = observer(() => {
     const rootStore = useMst();
 
     useEffect(() => {
@@ -17,11 +19,13 @@ export default function Home() {
             <h1>{rootStore.users.length}</h1>
             {rootStore.users.map(user => {
                 return (
-                    <>
+                    <div key={user.name}> {/* Add key to avoid warning */}
                         <h1>{user.name} - {user.age}</h1>
-                    </>
+                    </div>
                 )
             })}
         </div>
     );
-}
+});
+
+export default Home;
